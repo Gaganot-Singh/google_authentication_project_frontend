e1bb6174f2237b229bdd5ddfc690a1c2b25361d6
Gaganot-Singh <133317799+Gaganot-Singh@users.noreply.github.com>
2024-05-10 05:28:34 -0400

    error checking

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function HomePage({ isLoggedIn, name }) {
   } else {
     return (
       <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
-      <div className="p-8 max-w-md bg-white rounded-xl shadow-2xl border border-gray-200 flex flex-col items-center justify-center">
+        <div className="p-8 max-w-md bg-white rounded-xl shadow-2xl border border-gray-200 flex flex-col items-center justify-center">
             <h2 className="mb-6 text-3xl font-bold text-center text-gray-900">Log in to your account</h2>
             <button onClick={() => window.location.href = 'https://google-authentication-project-backend.onrender.com/auth/google'} type="button" className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mb-2">
                 <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
@@ -24,7 +24,6 @@ export default function HomePage({ isLoggedIn, name }) {
             </button>
         </div>
     </div>
-
     );
   }
 }
@@ -34,8 +33,12 @@ export async function getServerSideProps({ req }) {
   const isLoggedIn = !!cookies.jwt;
   let name = "";
   if (isLoggedIn) {
-    const decoded = jwt.verify(cookies.jwt, process.env.JWT_SECRET); 
-    name = decoded.name;
+    try {
+      const decoded = jwt.verify(cookies.jwt, process.env.JWT_SECRET);
+      name = decoded.name;
+    } catch (error) {
+      console.error('Token verification failed:', error);
+    }
   }
 
   return {
