b3abb031f90661bea60c0a3135d1f2416b56aa28
Gaganot-Singh <133317799+Gaganot-Singh@users.noreply.github.com>
2024-05-21 03:16:16 -0400

    improvements

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
-
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Link from 'next/link';
 export default function HomePage({ initialIsLoggedIn, initialName }) {
   const [isLoggedIn, setIsLoggedIn] = useState(initialIsLoggedIn);
   const [name, setName] = useState(initialName);
@@ -26,19 +28,40 @@ export default function HomePage({ initialIsLoggedIn, initialName }) {
     window.location.href = '/';
   };
 
-  if (isLoggedIn) {
+  if (!isLoggedIn) {
     return (
-      <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-400 to-blue-500">
-        <div className="p-6 bg-white shadow-lg rounded-lg border border-gray-200 items-center justify-center flex flex-col">
-          <h1 className="text-4xl font-bold text-gray-800">Welcome, {name}!</h1>
-          <button 
-            onClick={handleLogout} 
-            className="mt-4 text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5"
-          >
-            Logout
-          </button>
-        </div>
+      <>
+      <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-400 to-blue-500 flex-col">
+      <div className="p-6 bg-white shadow-lg rounded-lg border border-gray-200 flex flex-col items-center">
+        <h1 className="text-4xl font-bold text-gray-800">Welcome, {name}!</h1>
+        <button 
+          onClick={handleLogout} 
+          className="mt-4 text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5"
+        >
+          Logout
+        </button>
       </div>
+
+      <div className="p-6 bg-white shadow-lg rounded-lg border border-gray-200 mt-4 flex flex-col items-start" data-aos="fade-up"
+        data-aos-anchor-placement="center-bottom"
+        data-aos-duration="1500">
+        <h1 class="text-3xl font-bold text-gray-800 mb-4">Welcome to My Sample Google Authentication Project!</h1>
+        <p class="text-lg text-gray-700 mb-4">Thank you for signing up for my sample Google authentication project.</p>
+        <p class="text-lg text-gray-700 mb-4">This project was created as part of my practice to enhance my skills as a full-stack developer.</p>
+        <p class="text-gray-800 font-semibold mb-2">Project Features:</p>
+        <ul class="list-disc list-inside text-gray-700 mb-4">
+            <li>Google OAuth integration</li>
+            <li>Secure JWT authentication</li>
+            <li>Session management</li>
+            <li>User data handling with MongoDB</li>
+            <li>Logout functionality</li>
+        </ul>
+        <p class="text-lg text-gray-700 mb-4">I am continuously working on adding more features and improving the functionality.</p>
+        
+        <p class="text-lg text-gray-700"> To learn more about me and explore my other projects, visit: <Link href="https://gaganjot-singh.vercel.app/" target='blank' class="text-blue-500 underline"><b>Web Portfolio</b></Link>.</p>
+      </div>
+     </div>
+      </>
     );
   } else {
     return (
